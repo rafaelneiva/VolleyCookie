d54feb637b543607e096417d61cf03cbc174bd45
rafaelneiva <faelviolero@gmail.com>
2015-01-14 11:15:31 -0200

    Timeout option

--- app/src/main/java/br/com/newagemobile/volleycookie/AppController.java
+++ app/src/main/java/br/com/newagemobile/volleycookie/AppController.java
@@ -3,6 +3,7 @@ package br.com.newagemobile.volleycookie;
 import android.app.Application;
 import android.text.TextUtils;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.HttpClientStack;
@@ -60,7 +61,9 @@ public class AppController extends Application {
     public <T> void addToRequestQueue(Request<T> req, String tag) {
         // set the default tag if tag is empty
         req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-        getRequestQueue().add(req);
+        getRequestQueue().add(req).setRetryPolicy(new DefaultRetryPolicy(30000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)); // set timeout
     }
 
     public <T> void addToRequestQueue(Request<T> req) {
